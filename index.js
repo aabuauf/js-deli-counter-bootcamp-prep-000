ba8707a889d35f3d4bacbc2c16e6fb06b7035fbe
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 18:17:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(line,name){
   return "Welcome, "+ name +". You are number "+ (line.length) +" in line."
 }
 
-function nowserving(katzDeliLine){
+function nowServing(katzDeliLine){
   if (katzDeliLine[0]===""){
     return "There is nobody waiting to be served!"}
   return katzDeliLine[0];
