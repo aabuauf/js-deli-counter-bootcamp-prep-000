4751acc3e620aa12547e2ab411bad90c91695118
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 18:19:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function takeANumber(line,name){
 function nowServing(katzDeliLine){
   if (katzDeliLine[0]===""){
     return "There is nobody waiting to be served!"}
-  return katzDeliLine[0];
+  return "Currently serving" + katzDeliLine[0];
 }
